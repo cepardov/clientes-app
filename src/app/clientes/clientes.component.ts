e2d3fcc5361386fa63d4745445c6666a7bda3763
cepardov <wip015YkC1>
2019-01-23 12:29:46 -0300

    Se crea una lista de clientes en formato JSON y se lista en la vista correspondiente

---
+++ src/app/clientes/clientes.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Cliente } from './cliente';
+
+@Component({
+  selector: 'app-clientes',
+  templateUrl: './clientes.component.html',
+  styleUrls: ['./clientes.component.css']
+})
+export class ClientesComponent implements OnInit {
+
+  clientes: Cliente[] = [
+    {id: 1, nombre:'Cristian',apellido:'Pardo', createAt:'2018-12-30',email:'cepardov@gmail.com'},
+    {id: 2, nombre:'Victoria',apellido:'Bustamante', createAt:'2019-01-05',email:'vabustamantec@gmail.com'}
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
