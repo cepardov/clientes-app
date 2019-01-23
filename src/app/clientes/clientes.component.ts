1e9d7cb62ab12a6a7978372ca644b032f11de671
cepardov <wip015YkC1>
2019-01-23 13:46:41 -0300

    Se separa la capa de negocio creando un servicio ng g service cliente

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
+import { ClienteService } from './cliente.service';
 
 @Component({
   selector: 'app-clientes',
@@ -7,14 +8,12 @@ import { Cliente } from './cliente';
 })
 export class ClientesComponent implements OnInit {
 
-  clientes: Cliente[] = [
-    {id: 1, nombre:'Cristian',apellido:'Pardo', createAt:'2018-12-30',email:'cepardov@gmail.com'},
-    {id: 2, nombre:'Victoria',apellido:'Bustamante', createAt:'2019-01-05',email:'vabustamantec@gmail.com'}
-  ];
+  clientes: Cliente[];
 
-  constructor() { }
+  constructor(private clienteService: ClienteService) { }
 
   ngOnInit() {
+    this.clientes = this.clienteService.getClientes();
   }
 
 }
---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,6 @@
+import { Cliente } from "./cliente";
+
+export const CLIENTES: Cliente[] = [
+  {id: 1, nombre:'Cristian',apellido:'Pardo', createAt:'2018-12-30',email:'cepardov@gmail.com'},
+  {id: 2, nombre:'Victoria',apellido:'Bustamante', createAt:'2019-01-05',email:'vabustamantec@gmail.com'}
+];
